b27f0f71c7d0ca54d489884672c770a6b047ba41
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-03-09 11:17:06 +0000

    test code

---
+++ session 2/randomnumbergeneratormock.h
@@ -0,0 +1,11 @@
+#ifndef DP_EXAMPLE_RANDOMNUMBERGENERATORMOCK_H
+#define DP_EXAMPLE_RANDOMNUMBERGENERATORMOCK_H
+
+#include "gmock/gmock.h"
+
+class RandomNumberGeneratorMock {
+public:
+    //TODO
+};
+
+#endif //DP_EXAMPLE_RANDOMNUMBERGENERATORMOCK_H
